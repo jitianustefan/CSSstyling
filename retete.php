d53183f068e571f7e6cc60125cce6cf87ed939e3
jitianustefan <jitianu_stefan@yahoo.com>
2023-02-23 14:26:18 +0200

    upload CSS basic

---
+++ retete.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Retete</title>
+	<?php include("html/head.html"); ?>
+</head>
+<body>
+<div class="wrapper">
+	<?php include("html/header.html"); ?>
+	<?php include("html/retete.html"); ?>
+	<?php include("html/footer.html"); ?>
+</div>	
+</body>
+</html>
